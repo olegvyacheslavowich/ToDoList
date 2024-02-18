3429832bb0f5b750710cdef62c7f93db0eaff821
karpenko_ov <20_ok_13@mail.ru>
2024-02-19 01:50:34 +0500

    Less 6.6 - Fragment landscape

--- app/src/main/java/ru/elipson/todolist/presentation/MainActivity.kt
+++ app/src/main/java/ru/elipson/todolist/presentation/MainActivity.kt
@@ -1,23 +1,25 @@
 package ru.elipson.todolist.presentation
 
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.FragmentContainerView
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import ru.elipson.todolist.R
+import ru.elipson.todolist.domain.ToDoItem
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var viewModel: ToDoListViewModel
     private lateinit var toDoListAdapter: ToDoListAdapter
+    private var toDoItemLandscapeContainer: FragmentContainerView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        toDoItemLandscapeContainer = findViewById(R.id.toDoItemLandscapeContainer)
 
         setupRecyclerView()
 
@@ -28,8 +30,38 @@ class MainActivity : AppCompatActivity() {
 
         val buttonAddItem = findViewById<FloatingActionButton>(R.id.addFab)
         buttonAddItem.setOnClickListener {
+            openToDoItem()
+        }
+    }
+
+    private fun openToDoItemLandscapeFragment(item: ToDoItem? = null) {
+        val fragment = if (item == null) {
+            ToDoItemFragment.instanceAddItem()
+        } else {
+            ToDoItemFragment.instanceChangeItem(item.id)
+        }
+        supportFragmentManager.beginTransaction()
+            .add(R.id.toDoItemLandscapeContainer, fragment)
+            .commit()
+    }
+
+    private fun openToDoItemActivity(item: ToDoItem? = null) {
+        if (item == null) {
             val intent = ToDoItemActivity.newIntentAddItem(this)
             startActivity(intent)
+        } else {
+            val intent = ToDoItemActivity.newIntentEditItem(this, item.id)
+            startActivity(intent)
+        }
+    }
+
+    private fun isPortraitOrientation(): Boolean = toDoItemLandscapeContainer == null
+    private fun openToDoItem(item: ToDoItem? = null) {
+        if (isPortraitOrientation()) {
+            openToDoItemActivity(item)
+        } else {
+            openToDoItemLandscapeFragment(item)
+
         }
     }
 
@@ -39,8 +71,7 @@ class MainActivity : AppCompatActivity() {
             viewModel.changeEnabled(item)
         }
         toDoListAdapter.onClickListener = {
-            val intent = ToDoItemActivity.newIntentEditItem(this, it.id)
-            startActivity(intent)
+            openToDoItem(it)
         }
 
         recyclerView.adapter = toDoListAdapter
